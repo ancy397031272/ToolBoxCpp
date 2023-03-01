83e1065e5efb44f6f25c015093616ed1a1a8d499
AnchengWu <ac.wu@rvbust.com>
2023-03-01 15:51:52 +0800

    add install

---
+++ include/toolbox/FileSystem.h
@@ -0,0 +1,13 @@
+
+
+#ifndef TOOLBOXCPP_FILESYSTEM_H
+#define TOOLBOXCPP_FILESYSTEM_H
+
+namespace tb {
+    namespace utils {
+
+    }
+
+}
+
+#endif //TOOLBOXCPP_FILESYSTEM_H
---
+++ src/utils/FileSystem.cpp
@@ -0,0 +1,11 @@
+//
+// Created by rvbust on 3/1/23.
+//
+
+#include "FileSystem.h"
+
+namespace tb {
+    namespace utils {
+
+    }
+}
